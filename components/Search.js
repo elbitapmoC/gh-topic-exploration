2c3b59e78097190f51445476c53273f177ab0fbd
Ahkal <bp@sojourn.llc>
2022-08-12 05:15:53 -0400

    - and -> or

--- components/Search.js
+++ components/Search.js
@@ -28,7 +28,7 @@ const Search = () => {
           type="text"
           id="simple-search"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
-          placeholder="Search topics and repos"
+          placeholder="Search topics or repos"
           required=""
         />
       </div>
