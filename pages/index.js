364181e6f7327f72871446f349fc748139090f7f
Ahkal <bp@sojourn.llc>
2022-08-12 00:21:29 -0400

    - Save progress - Everything works as of now - No setbacks at this point MHW!

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import Heading from "../components/Heading";
 import Search from "../components/Search";
 import Title from "../components/Title";
-import Loading from "../components/Loading";
+import ButtonWithBadge from "../components/ButtonWithBadge";
 import GET_REPOS_AND_TOPICS from "../queries/getGithubTopic";
 import client from "../graphql-client";
 
@@ -11,14 +11,15 @@ export default function Home({ data, error }) {
   console.log(data);
   return (
     <>
-      <section className={styles.container}>
-        <Heading />
-        <main className={styles.main}>
-          <Title title="🚀 Github Explorer" />
-          <Search />
-        </main>
-        <Footer />
-      </section>
+      <Heading />
+      <main className={styles.main}>
+        <Title title="🚀 Github Explorer" />
+        <Search />
+        {data !== null && (
+          <ButtonWithBadge count={data.search.repositoryCount} />
+        )}
+      </main>
+      <Footer />
     </>
   );
 }
@@ -28,9 +29,7 @@ export async function getServerSideProps(ctx) {
   if (!queryString) {
     return { props: { data: null } };
   }
-
   console.log({ queryString });
-
   try {
     const results = await client.query({
       query: GET_REPOS_AND_TOPICS,
@@ -40,27 +39,4 @@ export async function getServerSideProps(ctx) {
   } catch {
     return { props: { data: null, error: "couldn't fetch from github" } };
   }
-
-  const { data, isLoading, error } = useQuery();
-
-  console.log(results.data);
-
-  // function RepoAndTopic({ queryString }) {
-  //   const { loading, error, data } = useQuery(GET_REPOS_AND_TOPICS, {
-  //     variables: { queryString },
-  //   });
-
-  //   if (loading) return <Loading />;
-  //   if (error) return `Error! ${error}`;
-  //   return (
-  //     <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
-  //   );
-  // }
-
-  // if (loading) return <Loading />;
-  // if (error) return `Error! ${error.message}`;
-  // console.log(data);
-  return {
-    props: {}, // will be passed to the page component as props
-  };
 }
